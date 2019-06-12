b103f0bba0ad6f41da54f97b5b3f0009e2551f77
John Winters <jhnwin@amazon.com>
2019-06-12 13:32:54 -0700

    commit new functions and testing and readme

---
+++ src/main/java/com/johnnywinters/java401d/songr/SongrController.java
@@ -0,0 +1,35 @@
+package com.johnnywinters.java401d.songr;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class SongrController {
+
+    // Specify the route for this method
+    @GetMapping("/hello")
+    public String getHelloWorld(){
+        return "Hello, World!";
+    }
+
+    @GetMapping("/capitalize/{sentence}")
+    public String capitalizePath(@PathVariable String sentence){
+        if(sentence == null) return null;
+        return sentence.toUpperCase();
+    }
+
+    @RequestMapping(value = "/reverse", method = RequestMethod.GET)
+    @ResponseBody
+    public String reverseSentence(
+            @RequestParam("sentence") String sentence) {
+        if(sentence == null) return null;
+        String[] sentenceSplit = sentence.split(" ");
+        StringBuilder returnSentence = new StringBuilder();
+        for(int i = (sentenceSplit.length - 1); i >= 0; i--){
+            returnSentence.append(sentenceSplit[i]);
+            if(i != 0){
+                returnSentence.append(" ");
+            }
+        }
+        return returnSentence.toString();
+    }
+}
---
+++ src/test/java/com/johnnywinters/java401d/songr/SongrControllerTest.java
@@ -0,0 +1,41 @@
+package com.johnnywinters.java401d.songr;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SongrControllerTest {
+
+    @Test
+    public void test_getHelloWorld() {
+        SongrController sc = new SongrController();
+        assertEquals("Hello, World!",
+                sc.getHelloWorld());
+    }
+
+    @Test
+    public void test_capitalizePath() {
+        SongrController sc = new SongrController();
+        assertEquals("HELLO, WORLD!",
+                sc.capitalizePath("Hello, World!"));
+    }
+
+    @Test
+    public void test_capitalizePath_null() {
+        SongrController sc = new SongrController();
+        assertNull(sc.capitalizePath(null));
+    }
+
+    @Test
+    public void test_reverseSentence() {
+        SongrController sc = new SongrController();
+        assertEquals("World! Hello,",
+                sc.reverseSentence("Hello, World!"));
+    }
+
+    @Test
+    public void test_reverseSentence_null() {
+        SongrController sc = new SongrController();
+        assertNull(sc.reverseSentence(null));
+    }
+}
