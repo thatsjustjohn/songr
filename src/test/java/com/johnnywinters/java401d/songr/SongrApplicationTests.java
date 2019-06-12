66eb97a2df515825ee95116a539210034f6ba00f
John Winters <jhnwin@amazon.com>
2019-06-12 11:51:30 -0700

    initial commit from Spring init

---
+++ src/test/java/com/johnnywinters/java401d/songr/SongrApplicationTests.java
@@ -0,0 +1,16 @@
+package com.johnnywinters.java401d.songr;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SongrApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
