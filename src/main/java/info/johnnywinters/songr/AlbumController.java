a571dde0ed0d416c7c92f569244e99203de903d1
John Winters <jhnwin@amazon.com>
2019-06-13 13:23:50 -0700

    made the site and add / display forms

---
+++ src/main/java/info/johnnywinters/songr/AlbumController.java
@@ -0,0 +1,34 @@
+package info.johnnywinters.songr;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
+public class AlbumController {
+    @Autowired
+    AlbumRepository albumRepository;
+
+    @GetMapping("/albums")
+    public String getAllAlbums(Model m){
+        Iterable<Album> albums = albumRepository.findAll();
+        m.addAttribute("albums", albums);
+        return "allAlbums";
+    }
+
+    @GetMapping("/albums/new")
+    public String getAddAlbumForm(){
+        return "albumForm";
+    }
+
+    @PostMapping("/albums")
+    public RedirectView addAlbums(@RequestParam String title, @RequestParam String artist, @RequestParam int songCount, @RequestParam int length, @RequestParam String imageUrl){
+        Album album = new Album(title, artist, songCount, length, imageUrl);
+        albumRepository.save(album);
+        return new RedirectView("/albums");
+    }
+}
